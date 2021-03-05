b0873d00f9c3b77020cab0739cc43694641d6c57
Pooya Parsa <pyapar@gmail.com>
2021-03-05 18:28:34 +0100

    fix: support prepaths as well and move to module entry

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import { resolve } from 'path'
 import { satisfies } from 'semver'
 import { name, version } from '../package.json'
 
-const pkgDir = resolve(__dirname, '..')
-
-// @ts-ignore
-global.__NUXT_PATHS__ = (global.__NUXT_PATHS__ || [])
-  .concat(pkgDir)
-
 function postcss8Module () {
   const { nuxt } = this
   const nuxtVersion = nuxt.constructor.version || '0.0.0'
@@ -15,7 +9,15 @@ function postcss8Module () {
   if (!satisfies(nuxtVersion, expectedVersion)) {
     // eslint-disable-next-line no-console
     console.warn(`[nuxt-postcss8] postcss@8 is not compatible with current version of nuxt (${nuxtVersion}). Expected: ${expectedVersion}`)
+    return
   }
+
+  const pkgDir = resolve(__dirname, '..')
+
+  // @ts-ignore
+  global.__NUXT_PATHS__ = (global.__NUXT_PATHS__ || []).concat(pkgDir)
+  // @ts-ignore
+  global.__NUXT_PREPATHS__ = (global.__NUXT_PREPATHS__ || []).concat(pkgDir)
 }
 
 postcss8Module.meta = {
