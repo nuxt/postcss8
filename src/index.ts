b6b737b68c6d8b0cc0e2754162131007fa5ee85f
Pooya Parsa <pyapar@gmail.com>
2021-03-05 19:59:40 +0100

    fix: early return and edge support

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,11 @@ import { name, version } from '../package.json'
 
 function postcss8Module () {
   const { nuxt } = this
-  const nuxtVersion = nuxt.constructor.version || '0.0.0'
+  const nuxtVersion = (nuxt.constructor.version || '0.0.0').split('-')[0]
   const expectedVersion = '>=2.15.0'
   if (!satisfies(nuxtVersion, expectedVersion)) {
     // eslint-disable-next-line no-console
     console.warn(`[nuxt-postcss8] postcss@8 is not compatible with current version of nuxt (${nuxtVersion}). Expected: ${expectedVersion}`)
-    return
   }
 
   const pkgDir = resolve(__dirname, '..')
