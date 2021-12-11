ad6029f170186833a4c1d8779cb3d1e73fcc321d
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2021-12-11 09:14:04 +0530

    fix: Nuxt3 version support

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { name, version } from '../package.json'
 
 function postcss8Module () {
   const { nuxt } = this
-  const nuxtVersion = (nuxt.constructor.version || '0.0.0').split('-')[0]
+  const nuxtVersion = (nuxt.constructor.version || nuxt._version || '0.0.0').split('-')[0]
   const expectedVersion = '>=2.15.3'
   if (!satisfies(nuxtVersion, expectedVersion)) {
     throw new Error(`postcss@8 is not compatible with current version of nuxt (${nuxtVersion}). Expected: ${expectedVersion}`)
